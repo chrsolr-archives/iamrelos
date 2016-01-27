16bcb4674d5280fc0c63d981c1239a8cc69635b5
Christian Soler <twenty40@gmail.com>
2016-01-26 18:59:08 -0600

    Live...

--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -1,6 +1,7 @@
 ///<reference path="../../../typings/tsd.d.ts" />
 var app;
 (function (app) {
+    var controllers;
     (function (controllers) {
         var HomeController = (function () {
             function HomeController(initData) {
@@ -16,8 +17,8 @@ var app;
             HomeController.$inject = ['initData'];
             return HomeController;
         })();
-
-        angular.module('app.controllers').controller('HomeController', HomeController);
-    })(app.controllers || (app.controllers = {}));
-    var controllers = app.controllers;
+        angular.module('app.controllers')
+            .controller('HomeController', HomeController);
+    })(controllers = app.controllers || (app.controllers = {}));
 })(app || (app = {}));
+//# sourceMappingURL=home-controller.js.map
