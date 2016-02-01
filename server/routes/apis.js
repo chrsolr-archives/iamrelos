cb4a691b980d72b9e211faea96ec16367923bbbf
Christian Soler <iamrelos@gmail.com>
2016-02-01 19:03:50 +0000

    Working on blogs api

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -1,3 +1,4 @@
+var request = require('request');
 var Quote = require('../models/quote');
 var Blog = require('../models/blog');
 
@@ -8,12 +9,22 @@ module.exports = function (app, express) {
     app.use('/api', api);
 
     api.get('/quotes', function (req, res) {
-
-        Quote.findOne({}, function(err, data) {
-            if (err) throw err;
-
-            return res.status(200).send(data);
-        })
+        // NOTE: Get random programming quote from this url: http://quotes.stormconsultancy.co.uk/random.json
+        // returns: (id: int), (author: string), (quote: string), (permalink: string)
+        var url = 'http://quotes.stormconsultancy.co.uk/random.json';
+        
+        request(url, function (error, response, body) {
+            if (error && response.statusCode !== 200) throw error;
+            
+            var json = JSON.parse(response.body);
+            
+            var quote = {
+                quote: json.quote,
+                author: json.author
+            };
+            
+            return res.status(200).send(quote);
+        });
     });
 
     api.get('/blogs', function(req, res){
